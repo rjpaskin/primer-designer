144ca0d469624ddc14463edceeed748e07610c0c
Rob Paskin <rjpaskin+git@gmail.com>
2012-06-15 17:06:18 +0100

    Tidy up SequenceSet

--- js/models/SequenceSet.js
+++ js/models/SequenceSet.js
@@ -4,11 +4,13 @@
     this.dna = new Bio.DNASequence(dna);
     
     this.complement = this.dna.complement();
-    this.protein = this.dna.translate();
+    this.protein    = this.dna.translate();
+    
     this.marker = {
       dna: this.ruler(this.dna),
       protein: this.ruler(this.protein, 30)
     };
+    
     this.title = title || '';
     if (title instanceof Array) { this.title = title[1]; }
   };
@@ -22,17 +24,17 @@
       str = str.toString();
     }
     
-    var count = Math.floor(str.length / 10), // how many numbers to output
-        nums  = '';
+    var count   = Math.floor(str.length / 10), // how many numbers to output
+        output  = '';
   
     for (var i = 1; i <= count; i++) {
       var pad_n = padding - i.toString().length - 1,
-          text  = '<span class="marker-group"><span class="spacer">'
-                  + (new Array(parseInt(pad_n) + 1).join('&nbsp;'))
-                  + '</span><span class="number">' + i + '<u>0</u></span></span>';
-      nums += text;
+          text  = new Array(parseInt(pad_n) + 1).join('&nbsp;');
+          
+      output += '<span class="marker-group"><span class="spacer">' + text
+                + '</span><span class="number">' + i + '<u>0</u></span></span>';
     }
   
-    return nums;
+    return output;
   };
 }());
